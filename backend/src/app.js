1092ff748c2c35ec9873604c8ecc8d705dc2d7a2
Anshuman-x49 <anshumannayak169@gmail.com>
2026-08-23 19:17:27 +0530

    feat: initialize Express application with CRUD endpoints for notes management

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 })
 
 app.post("/addnote", async (req, res) => {
