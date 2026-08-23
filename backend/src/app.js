d7b63257a08bca69250ed632749ca58a81c7afc7
Anshuman-x49 <anshumannayak169@gmail.com>
2026-08-23 19:15:50 +0530

    fix not found issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,10 @@ app.use(express.json());
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+
 app.post("/addnote", async (req, res) => {
   const { title, description, isFavorite } = req.body;
 
