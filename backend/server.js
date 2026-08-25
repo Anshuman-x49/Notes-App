f7d622d732e8f246da4c36cee901a5faf4086fd4
Anshuman-x49 <anshumannayak169@gmail.com>
2026-08-25 21:17:18 +0530

    database connection issue solved

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
+connectDB();
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
-  connectDB();
   console.log(`Server is running on ${port}`);
 });
