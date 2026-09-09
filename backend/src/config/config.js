4a180c902b00f3ef2e08c96d42b408e2e0aad3be
Anshuman-x49 <anshumannayak169@gmail.com>
2026-09-09 17:57:41 +0530

    marker for env

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -8,6 +8,13 @@ const config = {
     refresh_token_secret: process.env.REFRESH_TOKEN_SECRET,
 };
 
+console.log("Environmental variables loading : ", {
+    db: config.db_url ? '✓' : '✗',
+    port: config.port ? '✓' : '✗',
+    accessToken: config.access_token_secret ? '✓' : '✗',
+    refreshToken: config.refresh_token_secret ? '✓' : '✗'
+});
+
 if (!config.db_url || !config.port || !config.access_token_secret || !config.refresh_token_secret) {
     throw new Error("Please provide all the required environment variables");
 }
