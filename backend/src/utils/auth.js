15e392c284a363807156d15a3931c99f46ae69b7
Anshuman-x49 <anshumannayak169@gmail.com>
2026-09-09 18:21:08 +0530

    fix the auth cotrollers and saving refresh token hash in db

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 
-export function generateToken({id}) {
-    const accessToken = jwt.sign(id, config.access_token_secret, { expiresIn: "15m" });
-    const refreshToken = jwt.sign(id, config.refresh_token_secret, { expiresIn: "7d" });
+export function generateToken({ id }) {
+    const accessToken = jwt.sign({ id }, config.access_token_secret, { expiresIn: "15m" });
+    const refreshToken = jwt.sign({ id }, config.refresh_token_secret, { expiresIn: "7d" });
     return { accessToken, refreshToken };
 }
